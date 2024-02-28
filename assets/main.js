a957442f0b34948f8d7726484c0fc96122b5bfee
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-02-28 17:14:56 +0100

    filled my array with api call from storejs

--- assets/main.js
+++ assets/main.js
@@ -9,7 +9,7 @@ createApp({
         }
     },
     mounted() {
-
+        this.disksCall()
     },
     methods: {
         disksCall() {
--- server.php
+++ server.php
@@ -3,7 +3,6 @@ $string = file_get_contents("store.json");
 
 $list = json_decode($string, true);
 
-var_dump($list);
 
 header('Content-Type: application/json');
 
