2ee0dda43c8bf1967e57999c2dba600a27a122cd
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-02-28 17:04:37 +0100

    decoding and encoding json

--- server.php
+++ server.php
@@ -1,2 +1,10 @@
 <?php
 $string = file_get_contents("store.json");
+
+$list = json_decode($string, true);
+
+var_dump($list);
+
+header('Content-Type: application/json');
+
+echo json_encode($list);
